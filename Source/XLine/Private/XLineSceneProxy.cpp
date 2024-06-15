489c0922175b000fc2f5d86d514d8a298913ceff
springdong <springu3d@yeah.net>
2024-06-15 14:38:21 +0800

    update : shader folder;

--- Source/XLine/Private/XLineSceneProxy.cpp
+++ Source/XLine/Private/XLineSceneProxy.cpp
@@ -1,8 +1,7 @@
 ﻿#include "XLineSceneProxy.h"
 #include "XLineComponent.h"
 
-FXLineSceneProxy::FXLineSceneProxy(UXLineComponent* PrimitiveComponent)
-	: FPrimitiveSceneProxy(PrimitiveComponent)
+FXLineSceneProxy::FXLineSceneProxy(UXLineComponent* PrimitiveComponent) : FPrimitiveSceneProxy(PrimitiveComponent)
 {
 	
 }
@@ -10,4 +9,14 @@ FXLineSceneProxy::FXLineSceneProxy(UXLineComponent* PrimitiveComponent)
 FXLineSceneProxy::~FXLineSceneProxy()
 {
 	
+}
+
+void FXLineSceneProxy::GetDynamicMeshElements(const TArray<const FSceneView*>& Views, const FSceneViewFamily& ViewFamily, uint32 VisibilityMap, FMeshElementCollector& Collector) const
+{
+	FPrimitiveSceneProxy::GetDynamicMeshElements(Views, ViewFamily, VisibilityMap, Collector);
+}
+
+FPrimitiveViewRelevance FXLineSceneProxy::GetViewRelevance(const FSceneView* View) const
+{
+	return FPrimitiveSceneProxy::GetViewRelevance(View);
 }
--- Source/XLine/Private/XLineSceneProxy.h
+++ Source/XLine/Private/XLineSceneProxy.h
@@ -8,4 +8,7 @@ public:
 
 	FXLineSceneProxy(UXLineComponent* PrimitiveComponent);
 	virtual ~FXLineSceneProxy();
+
+	virtual void GetDynamicMeshElements(const TArray<const FSceneView*>& Views, const FSceneViewFamily& ViewFamily, uint32 VisibilityMap, FMeshElementCollector& Collector) const override;
+	virtual FPrimitiveViewRelevance GetViewRelevance(const FSceneView* View) const override;
 };
